1621f0ba14b8cdc25fa6bac129c71c41580324e0
Guilherme <guilhermed.dev@gmail.com>
2025-08-14 11:57:28 -0300

    fix(ProductAPI/ProductController): fix logic problem on update method and format code

--- ProductAPI/Controllers/ProductController.cs
+++ ProductAPI/Controllers/ProductController.cs
@@ -6,30 +6,23 @@ namespace ProductAPI.Controllers;
 
 [ApiController]
 [Route("api/v1/[controller]")]
-public class ProductController : Controller
+public class ProductController(ILogger<ProductController> logger, IProductRepository productRepository) : Controller
 {
-    private readonly ILogger<ProductController> _logger;
-    private readonly IProductRepository _productRepository;
-
-    public ProductController(ILogger<ProductController> logger, IProductRepository productRepository)
-    {
-        _logger = logger;
-        _productRepository = productRepository;
-    }
-
     [HttpGet]
     public ActionResult<IEnumerable<ProductDTO>> FindAll()
     {
-        _logger.LogInformation("Fetching all products");
-        var products = _productRepository.FindAll();
+        logger.LogInformation("Fetching all products");
+
+        var products = productRepository.FindAll();
         return Ok(products);
     }
 
     [HttpGet("{id}")]
     public ActionResult<ProductDTO> FindById(long id)
     {
-        _logger.LogInformation($"Fetching product with ID: {id}");
-        var product = _productRepository.FindById(id);
+        logger.LogInformation($"Fetching product with ID: {id}");
+
+        var product = productRepository.FindById(id);
         return product != null ? Ok(product) : NotFound();
     }
 
@@ -37,8 +30,10 @@ public class ProductController : Controller
     public ActionResult<ProductDTO> Create(ProductDTO dto)
     {
         if (dto == null) return BadRequest();
-        _logger.LogInformation("Creating a product");
-        var product = _productRepository.Create(dto);
+
+        logger.LogInformation("Creating a product");
+
+        var product = productRepository.Create(dto);
         return Ok(product);
     }
 
@@ -46,16 +41,19 @@ public class ProductController : Controller
     public ActionResult<ProductDTO> Update(ProductDTO dto)
     {
         if (dto == null) return BadRequest();
-        _logger.LogInformation($"Updating a product");
-        var product = _productRepository.Update(dto);
-        return product == null ? Ok(product) : NotFound();
+
+        logger.LogInformation($"Updating a product");
+
+        var product = productRepository.Update(dto);
+        return product != null ? Ok(product) : NotFound();
     }
 
     [HttpDelete("{id}")]
     public ActionResult Delete(long id)
     {
-        _logger.LogInformation($"Deleting product with ID: {id}");
-        var status = _productRepository.Delete(id);
+        logger.LogInformation($"Deleting product with ID: {id}");
+
+        var status = productRepository.Delete(id);
         return status ? Ok(status) : BadRequest();
     }
 }
