f770865efc6c7fe051fb2894b7920732660ed53e
Guilherme <guilhermed.dev@gmail.com>
2025-08-14 12:09:11 -0300

    feat(Frontend): add new methods on IProductService

--- Frontend/Services/IProductService.cs
+++ Frontend/Services/IProductService.cs
@@ -5,4 +5,8 @@ namespace Frontend.Services;
 public interface IProductService
 {
     Task<IEnumerable<ProductViewModel>> FindAllProducts();
+    Task<ProductViewModel> FindProductById(long id);
+    Task<ProductViewModel> CreateProduct(ProductViewModel model);
+    Task<ProductViewModel> UpdateProduct(ProductViewModel model);
+    Task<bool> DeleteProductById(long id);
 }
